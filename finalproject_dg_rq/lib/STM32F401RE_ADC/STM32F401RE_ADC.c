770c4d8d621dda3bd75ea5eebf6f5e62c86616e1
rebeccaqin <rebeccafqin@gmail.com>
2020-11-03 20:17:49 -0600

    added ADC struct

---
+++ finalproject_dg_rq/lib/STM32F401RE_ADC/STM32F401RE_ADC.c
@@ -0,0 +1,65 @@
+// STM32F401RE_ADC.h
+// Header for ADC functions
+
+#ifndef STM32F4_ADC_H
+#define STM32F4_ADC_H
+
+#include <stdint.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Definitions
+///////////////////////////////////////////////////////////////////////////////
+
+#define __IO volatile
+
+// Base addresses for GPIO ports
+#define ADC_BASE (0x40012000UL) // base address of RCC
+
+///////////////////////////////////////////////////////////////////////////////
+// Bitfield structs
+///////////////////////////////////////////////////////////////////////////////
+
+typedef struct {
+  __IO uint32_t LATENCY :4;
+  __IO uint32_t         :4;
+  __IO uint32_t PRFTEN  :1;
+  __IO uint32_t ICEN    :1;
+  __IO uint32_t DCEN    :1;
+  __IO uint32_t ICRST   :1;
+  __IO uint32_t DCRST   :1;
+  __IO uint32_t         :19;
+} ADCSR_bits;
+
+typedef struct {
+  __IO ADCSR_bits ADCSR;      /*!< ADC status register,   Address offset: 0x00 */
+  __IO uint32_t ADCCR1;     /*!< ADC control register 1,              Address offset: 0x04 */
+  __IO uint32_t ADCCR2;  /*!< ADC control register 2,       Address offset: 0x08 */
+  __IO uint32_t ADCSMPR1;       /*!< ADC sample time register 1,           Address offset: 0x0C */
+  __IO uint32_t ADCSMPR2;       /*!< ADC sample time register 2,          Address offset: 0x10 */
+  __IO uint32_t ADCJOFR1;    /*!< ADC injected channel data offset register 1,  Address offset: 0x14 */
+  __IO uint32_t ADCJOFR2;    /*!< ADC injected channel data offset register 2,  Address offset: 0x18 */
+  __IO uint32_t ADCJOFR3;    /*!< ADC injected channel data offset register 3,  Address offset: 0x1C */
+  __IO uint32_t ADCJOFR4;    /*!< ADC injected channel data offset register 4,  Address offset: 0x20 */
+  __IO uint32_t ADCHTR;   /*!< ADC watchdog higher threshold register Address offset: 0x24 */
+  __IO uint32_t ADCLTR;   /*!< ADC watchdog lower threshold register Address offset: 0x28 */
+  __IO uint32_t ADCSQR1;   /*!< ADC regular sequence register Address offset: 0x28 */
+  __IO uint32_t ADCSQR1;   /*!< ADC regular sequence register Address offset: 0x30 */
+  __IO uint32_t ADCSQR3;   /*!< ADC regular sequence register Address offset: 0x34 */
+  __IO uint32_t ADCJSQR;   /*!< ADC injected sequence register Address offset: 0x38 */
+  __IO uint32_t ADCJDR1;    /*!< ADC injected data register 1,  Address offset: 0x3C */
+  __IO uint32_t ADCJDR2;    /*!< ADC injected data register 2,  Address offset: 0x40 */
+  __IO uint32_t ADCJDR3;    /*!< ADC injected data register 3,  Address offset: 0x44 */
+  __IO uint32_t ADCJDR4;    /*!< ADC injected data register 4,  Address offset: 0x48 */
+  __IO uint32_t ADCDR;    /*!< ADC regular data register,  Address offset: 0x4C */
+  __IO uint32_t ADCCCR;    /*!< ADC common control register,  Address offset: 0x04 */
+} ADC_TypeDef;
+
+#define ADC ((ADC_TypeDef *) ADC_BASE)
+
+///////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+///////////////////////////////////////////////////////////////////////////////
+
+void configureADC();
+
+#endif
